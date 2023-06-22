557e63b356ddf761e372eb7973cfcb58ba90031c
Evan Lin <evan.if.lin@linecorp.com>
2023-06-22 19:04:28 +0800

    feat: add ProductTool for searching products to agent initialization
    - Add a ProductTool for searching products
    - Include the new ProductTool in the initialize_agent function
    - Import the necessary modules for the new ProductTool in main.py
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from langchain.schema import HumanMessage
 from poi import TravelPOITool
 from ticket import TravelTicketTool
 from exp import TravelExpTool
+from product import ProductTool
 
 # CWB Weather Tools
 from weather import WeatherDataTool
@@ -66,7 +67,7 @@ parser = WebhookParser(channel_secret)
 # Langchain (you must use 0613 model to use OpenAI functions.)
 model = ChatOpenAI(model="gpt-3.5-turbo-0613")
 tools = [TravelPOITool(), TravelTicketTool(),
-         TravelExpTool(), WeatherDataTool()]
+         TravelExpTool(), WeatherDataTool(), ProductTool()]
 
 open_ai_agent = initialize_agent(tools,
                                  model,
